b3eb22a700098be95789c5d9a5bc40ace3249cb2
Jacek Chowaniec <jacek.chowaniec85@gmail.com>
2022-02-12 13:05:45 +0100

    simple assertion

--- SeleniumTest.py
+++ SeleniumTest.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import unittest
 
@@ -12,6 +13,11 @@ class SeleniumTest(unittest.TestCase):
 
     # // when
     driver.get('https://modivo.pl/')
+    element = driver.find_element(By.CLASS_NAME, 'marketing-bar-global')
+    is_displayed = element.is_displayed()
 
     # // then
     driver.close()
+    print('=====================')
+    print(is_displayed)
+    print('=====================')
